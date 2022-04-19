33df7bb6e57c42b9eeee766b0626ffd54b6a0310
greyliath <kbroussardco@gmail.com>
2022-04-19 13:42:46 +0100

    Removes unecessary code from creating react app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='container'>
       Hello world
+      <footer><a href="https://github.com/greyliath/react-dictionary" target="_blank" rel="noreferrer">open sourced app</a> from greyliath</footer>
     </div>
+    </div>
+    
   );
 }
 
